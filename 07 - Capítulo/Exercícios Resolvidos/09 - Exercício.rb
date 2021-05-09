23d93d39089cd54d97b96f6b7a1ad4c152e2ce74
Rafael Costa <rafael.costta13@outlook.com>
2021-05-09 18:28:49 -0300

    Criando todos os arquivos e diretórios

---
+++ 07 - Capítulo/Exercícios Resolvidos/09 - Exercício.rb
@@ -0,0 +1,14 @@
+=begin
+	09 - Faça um programa que calcule e mostre a área de um triângulo.
+	Sabe-se que: Área = (base * altura) / 2
+=end
+
+puts "Digite a base do triângulo :"
+base = gets.to_f
+
+puts "Digite a altura do triângulo :"
+altura = gets.to_f
+
+area = (base * altura) / 2
+puts "A área do triângulo é : #{area}"
+
