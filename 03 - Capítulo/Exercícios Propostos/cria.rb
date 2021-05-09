d5a797fc6d6428beb3721707ac346ed7c9698269
Rafael costta <rafaelcoatta13>
2021-05-09 15:06:55 -0300

    Add exercicios

---
+++ 03 - Capítulo/Exercícios Propostos/cria.rb
@@ -0,0 +1,11 @@
+nome = "Exercicio.rb"
+
+for i in 2..25
+  if i < 10 
+    nome_file = '0' + i.to_s + ' - ' + nome
+  else
+    nome_file = i.to_s + ' - ' + nome
+  end
+  File.new(nome_file, "w")
+  puts nome_file
+end
