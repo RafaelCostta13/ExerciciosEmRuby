9cc696f24f9ab9abf532a76d8da44e411fe091b8
Rafael Costa <rafael.costta13@outlook.com>
2021-05-10 22:49:44 -0300

    upload

---
+++ 04  -  Capítulo/Exercícios Resolvidos/13 - Exercicio.rb
@@ -0,0 +1,32 @@
+=begin
+	13 - Faça um programa que apresente o menu a seguir, permita ao usuário escolher a opção desejada, re-
+	ceba os dados necessários para executar a operação e mostre o resultado. Verifique a possibilidade de
+	opção inválida e não se preocupe com restrições, como salário negativo.
+		Menu de opções:
+		1. Imposto
+		2. Novo salário
+		3. Classificação
+	Digite a opção desejada.
+	
+	Na opção 1: receber o salário de um funcionário, calcular e mostrar o valor do imposto usando as regras
+	a seguir.
+	SALARIO	PERCENTUAL DO IMPOSTO
+	< 500	- 5%
+	>=500	- <=850 10%
+	> 850	- 15%	
+
+	Na opção 2: receber o salário de um funcionário, calcular e mostrar o valor do novo salário, usando as
+	regras a seguir.
+	SALARIO	AUMENTO
+	> 1500	-	R$ 25
+	>=750 <= 1500 - R$ 50
+	>=450 <750	- R$ 75
+	< 450	- R$ 100
+
+	Na opção 3: receber o salário de um funcionário e mostrar sua classificação usando a tabela a seguir.
+	SALARIO		Aumento
+> 1500	-	R$ 25
+>= 750 <= 1500	- R$ 50
+>=450 < 740	-	75
+< 450	-	R$ 100
+=end
